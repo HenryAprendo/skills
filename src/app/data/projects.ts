0cf869d96f19804c928c702d23eda23ee8676b3f
HenryAprendo <hjsmipuc@gmail.com>
2023-07-26 07:48:15 -0500

    Modificación del path de las imagenes para que sea calculado desde la carpeta raiz en desarrollo o produccion

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -13,63 +13,63 @@ export const projects:Projects[] = [
     title: 'Formulary multi step',
     skills: ['Angular','Tailwindcss','Html','Git','Mobile first'],
     url: 'https://multi-step-form-800a8.web.app',
-    image: './../../assets/images/form-steps.jpg'
+    image: './assets/images/form-steps.jpg'
   },
   {
     id: 7,
     title: 'Countries world',
     skills: ['Angular','Tailwindcss','Html','Git','Mobile first'],
     url: 'https://countries-world-921d1.web.app',
-    image: './../../assets/images/country.jpg'
+    image: './assets/images/country.jpg'
   },
   {
     id: 6,
     title: 'Jobs listing',
     skills: ['Angular','CSS','HTML','Git','Mobile first'],
     url: 'https://jobs-list-d37e3.web.app/',
-    image: './../../assets/images/jobs.jpg'
+    image: './assets/images/jobs.jpg'
   },
   {
     id: 5,
     title: 'Todo app',
     skills: ['React','CSS','HTML','Git','Mobile first'],
     url: 'https://todo-app-fa1dc.web.app',
-    image: './../../assets/images/todo-react.jpg'
+    image: './assets/images/todo-react.jpg'
   },
   {
     id: 4,
     title: 'Fashion store',
     skills: ['Angular','Rxjs','Tailwindcss','Html','Git','Mobile first'],
     url: 'https://fashion-store-956b2.web.app/home',
-    image: './../../assets/images/store.jpg'
+    image: './assets/images/store.jpg'
   },
   {
     id: 3,
     title: 'The bible',
     skills: ['Angular','Rxjs','Html','Css','Git','Mobile first'],
     url: 'https://the-bible-95f37.web.app/bible',
-    image: './../../assets/images/bible.jpg'
+    image: './assets/images/bible.jpg'
   },
   {
     id: 2,
     title: 'Sunnyside agency',
     skills: ['Html','Tailwindcss','Git','Mobile first', 'Responsive design'],
     url: 'https://sunnyside-agency-3d1df.web.app/',
-    image: './../../assets/images/sunnyside.jpg'
+    image: './assets/images/sunnyside.jpg'
   },
   {
     id: 1,
     title: 'Todo drag and drog',
     skills: ['Angular','Tailwindcss','Rxjs','Html','Git','Mobile first'],
     url: 'https://to-do-list-d126c.web.app/',
-    image: './../../assets/images/todo.jpg'
+    image: './assets/images/todo.jpg'
   },
   {
     id: 0,
     title: 'Text encryptor',
     skills: ['Javascript','Html','Css','Git','Mobile first'],
     url: 'https://henryaprendo.github.io/Encriptador-Alura-RD/',
-    image: './../../assets/images/encriptador.jpg',
+    image: './assets/images/encriptador.jpg',
   },
 ];
 
