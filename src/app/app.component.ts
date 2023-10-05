22de7bf2c31b9da32e345eb68f133ff636c7be9b
HenryAprendo <hjsmipuc@gmail.com>
2023-10-05 15:36:27 -0500

    Libreria para el manejo dinamico de fechas, que determina el tiempo de experiencia en cada habilidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { RouterOutlet } from '@angular/router';
 import { Technologies, technologies} from './data/skills';
 import { Projects, projects } from './data/projects';
 import { SocialComponent } from './components/social/social.component';
+import {intervalToDuration, differenceInCalendarYears, differenceInCalendarMonths, differenceInYears } from 'date-fns'
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -17,4 +20,34 @@ export class AppComponent {
 
   projectList = projects.slice();
 
+  calculateDateDifference(start:Date){
+    let value = intervalToDuration({
+      start: start,
+      end: new Date()
+    });
+
+    if(value && value.years && value.months){
+      let years = value.years;
+      let months = value.months;
+
+      if( years > 0 && months > 0){
+        return `${value.years} year and ${value.months} months`
+      } else if(years > 0){
+        return `${value.years} year`;
+      }
+
+    }
+
+    return `${value.months} months`;
+
+  }
+
 }
+
+
+
+
+
+
+
+
--- src/app/data/skills.ts
+++ src/app/data/skills.ts
@@ -2,48 +2,48 @@
 export interface Technologies {
   id: number;
   title: string;
-  experience: string;
+  date: Date
 }
 
 export const technologies:Technologies[] = [
   {
     id: 0,
     title: 'Javascript',
-    experience: '1 year and a half'
+    date: new Date(2021,10)
   },
   {
     id: 1,
     title: 'Typescript',
-    experience: '1.3 year'
+    date: new Date(2022,5)
   },  {
     id: 2,
     title: 'Angular',
-    experience: '1.3 year'
+    date: new Date(2022,6)
   },  {
     id: 3,
     title: 'React',
-    experience: '3 months'
+    date: new Date(2023,3)
   },  {
     id: 4,
     title: 'HTML',
-    experience: '1 year and a half'
+    date: new Date(2021,11)
   },  {
     id: 5,
     title: 'CSS',
-    experience: '1 year and a half',
+    date: new Date(2021,11)
   },  {
     id: 6,
     title: 'TailwindCss',
-    experience: '9 months'
+    date: new Date(2023,0)
   },
   {
     id: 7,
     title: 'Mobile first',
-    experience: '1 years'
+    date: new Date(2022,2)
   },
   {
     id: 8,
     title: 'Java + springboot (fundamentals)',
-    experience: '6 month'
+    date: new Date(2023,5)
   }
 ];
